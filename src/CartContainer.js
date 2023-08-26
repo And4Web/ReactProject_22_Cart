57e1cfb2ec562c54ec69778764d3b403dd0a94d1
unknown <anands2807@gmail.com>
2023-08-26 19:32:18 +0530

    modified data.js

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import CartItem from './CartItem';
+import { useGlobalContext } from "./context";
 
 function CartContainer() {
+  const {cart} = useGlobalContext();
+  if(cart.length ===0){
+    return (
+      <section className='cart'>
+        <header>
+          <h2>Your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    )
+  }
   return (
-    <div>
-      <h1>Cart container</h1>
-      <CartItem/>
-    </div>
+    <section className='cart'>
+      <header>
+        <h2>Your bag</h2>
+      </header>
+      <div>
+        {cart.map(item=>{
+          return(
+            <CartItem key={item.id} {...item}/>
+          )
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            Total <span>$0.00</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={()=>console.log("clear cart")}>Clear cart</button>
+      </footer>
+    </section>
   )
 }
 
--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export default [
   {
     id: 1,
-    title: 'Samsung Galaxy S7',
-    price: 599.99,
-    img: 'https://dl.airtable.com/.attachments/91ee456448cef47deec553a2ea3fa8ad/b08bec68/phone-2_ohtt5s.png',
+    title: 'Samsung Galaxy S21',
+    price: 899.99,
+    img: 'https://t-mobile.scene7.com/is/image/Tmusprod/fg-samsung_galaxy_s21-5G-phantom-gray-nologo-5:1-to-1-ratio?ts=1678466226752&fmt=png-alpha&dpr=off',
     amount: 1,
   },
   {
     id: 2,
-    title: 'google pixel ',
-    price: 499.99,
-    img: 'https://dl.airtable.com/.attachments/91c88ae8c1580e2b762ecb3f73ed1eed/a633139a/phone-1_gvesln.png',
+    title: 'apple iphone 13',
+    price: 999.99,
+    img: 'https://pngimg.com/uploads/iphone_13/iphone_13_PNG2.png',
     amount: 1,
   },
   {
     id: 3,
-    title: 'Xiaomi Redmi Note 2',
-    price: 699.99,
-    img: 'https://dl.airtable.com/.attachments/bae9208dc34f35128749ecda5b999e84/337c285d/phone-3_h2s6fo.png',
+    title: 'Xiaomi Redmi Note 12',
+    price: 299.99,
+    img: 'https://mobileworldpirmahal.com/wp-content/uploads/2023/06/Xiaomi-Redmi-Note-12.png',
     amount: 1,
   },
 ];
